52eaae890f290f406a2c05c1e596d518f7346857
JOSE ALZUETA <TAITUSTITO@hotmail.com>
2019-03-11 16:54:39 -0300

    first commit

---
+++ Test/EmployeeTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using ScreenPlay.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenPlay.Test
+{
+    [TestFixture]
+    public class EmployeeTest : BaseTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Login.As(Driver, "admin", "admin123");
+        }
+
+        [Test]
+        public void AddEmployeeTest()
+        {
+            AddEmployee.WithTheData(Driver, "Juan", "juan@gmail.com","MTV","598752022");
+            Assert.IsTrue(AcceptAlert.Success(Driver));
+        }        
+    }
+}
---
+++ UI/EmployeeUI.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenPlay.UI
+{
+    public class EmployeeUI
+    {
+        public static By Form = By.Id("formEmployee");
+        public static By NameInput = By.XPath("//*[@id='formEmployee']/div[2]/div[1]/input");
+        public static By EmailInput = By.XPath("//*[@id='formEmployee']/div[2]/div[2]/input");
+        public static By AddressTextArea = By.Id("address");
+        public static By PhoneInput = By.Id("phone");
+        public static By AddButton = By.Id("addButton");
+    }
+}
